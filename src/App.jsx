975a05d39d5bac58e7802947c9aaee3799a3450b
Felipe Espinoza <felipe.espinoza2019@alu.uct.cl>
2024-05-29 22:00:19 -0400

    aa

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 h-[100vh]">
+      <h1 className="text-3xl font-bold underline">
+        HOla CHUPETON!!!
+      </h1>
+    </div>
+  )
+}
--- truffle-config.js
+++ truffle-config.js
@@ -46,7 +46,7 @@
 
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-module.exports = {
+export default {
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
